99d4f820b6326f4a94efddbada0221b8222e8c74
naveedwani <waninaveed01@gmail.com>
2024-07-31 09:52:44 +0530

    Update checkdiskusage.sh

--- checkdiskusage.sh
+++ checkdiskusage.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # This script checks disk usage and sends an alert if usage exceeds 80%
-
+echo "Checking disk usage..."
 USAGE=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
 
 if [ $USAGE -gt 80 ]; then
   echo "Warning: Disk usage is above 80%! Current usage: $USAGE%"
 fi
+
+echo "Disk usage check complete."
+
+
